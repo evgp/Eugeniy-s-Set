24559fe3921f688e490b008d86f54f4b16dd4501
Евгений <ep@macbook-pro-evgeniy.local>
2018-05-21 17:53:20 +1200

    no comments

--- Eugeniy's Set/ViewController.swift
+++ Eugeniy's Set/ViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        sender.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        cardButtons[10].backgroundColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
